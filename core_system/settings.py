08659913d62967e03208d141b7e01b7dfe99dba2
squaad99 <squaaad1993@gmail.com>
2020-06-01 00:03:20 +0200

    Version 13

--- core_system/settings.py
+++ core_system/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -48,7 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites'
 ]
 
 MIDDLEWARE = [
@@ -138,5 +136,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'core-home'
 
 LOGIN_URL = 'login'
-
-django_heroku.settings(locals())
