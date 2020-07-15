9ae0f1dbaa075a9898dd67ef402c77fae8c94e13
squaad99 <squaaad1993@gmail.com>
2020-07-15 19:29:51 +0200

    Test added

--- customers/tests/test_urls.py
+++ customers/tests/test_urls.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from customers.views import Overview
+from customers.views import Overview, Create
 
 
 class TestUrls(SimpleTestCase):
@@ -9,3 +9,7 @@ class TestUrls(SimpleTestCase):
     def test_customer_overview_is_resolved(self):
         url = reverse('customer-overview')
         self.assertEqual(resolve(url).func.view_class, Overview)
+
+    def test_customer_create_is_resolved(self):
+        url = reverse('customer-create')
+        self.assertEqual(resolve(url).func.view_class, Create)
