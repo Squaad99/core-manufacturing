b5d061ddf7062613b200cea3553c30bbe2d49899
Squaad99 <squaad1993@gmail.com>
2020-07-23 03:17:20 +0200

    Update calendar (#12)
    Co-authored-by: squaad99 <squaaad1993@gmail.com>

--- time_accounting/urls.py
+++ time_accounting/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from time_accounting.views import TimeReportCreate, TimeReportUpdate, TimeReportDelete, TimeCalendarOverView, \
-    WorkReportCreate, WorkReportDate
+    WorkReportCreate, TimeCalendarDate
 
 urlpatterns = [
     path('', TimeReportCreate.as_view(), name='time-overview'),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('<int:pk>/delete/', TimeReportDelete.as_view(), name='time-report-delete'),
     path('calendar/overview/', TimeCalendarOverView.as_view(), name='time-calendar-overview'),
     path('calendar/overview/<str:selected_month>/', TimeCalendarOverView.as_view(), name='time-calendar-overview'),
+    path('calendar/dateview/<str:selected_date>/', TimeCalendarDate.as_view(), name='time-calendar-dateview'),
     path('calendar/new/', WorkReportCreate.as_view(), name='time-calendar-create'),
     path('calendar/update/<str:pk>', WorkReportCreate.as_view(), name='time-calendar-update'),
-    path('calendar/date/<str:pk>', WorkReportDate.as_view(), name='time-calendar-date'),
 ]
