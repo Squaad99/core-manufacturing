7e1ed58d589d8fd6daad7f8af234d81f1d874b8e
Squaad99 <squaad1993@gmail.com>
2020-08-05 02:03:52 +0200

    Company tab fix (#16)
    * Upload for work on main comp
    
    * Recent evets fixed and correct tab for company
    
    Co-authored-by: Henrik Berglund <henrik.berglund@volvocars.com>
    Co-authored-by: squaad99 <squaaad1993@gmail.com>

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+from core.utils import get_most_recent_events
 from users.models import Company, Profile
 
 
@@ -10,14 +11,10 @@ class Home(LoginRequiredMixin, TemplateView):
     template_name = os.path.join('core', 'home.html')
 
     def get_context_data(self, **kwargs):
+        company = Profile.objects.get(user=self.request.user.id).company
         context = super().get_context_data(**kwargs)
 
-        events = [
-            'Projekt 1 uppdaterad 2',
-            'Projekt 1 skapad',
-            'Status 1 ändrad',
-        ]
-        context['events'] = events
+        context['events'] = get_most_recent_events(company)
 
         profile = Profile.objects.get(user=self.request.user.id)
         context['user_profile'] = profile
